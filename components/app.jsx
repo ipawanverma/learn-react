fb40acfd760c9f04291bcfd27c8f0b27df7984d4
ipawanverma <148688699+ipawanverma@users.noreply.github.com>
2026-09-21 12:11:09 +0530

    Update image source and enhance header description

--- components/app.jsx
+++ components/app.jsx
@@ -1,11 +1,20 @@
+import reactImg from './assets/react-core-concepts.png';                   // reactImg is used for fetching the image from this location and then later on used below,
+                                                                           // also we can give any name of our choice like reactImg etc.
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
 
 
 function Header() {
+  const description = reactDescriptions[genRandomInt(2)];
+
   return (<header>
-    <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+    <img src={reactImg} alt="Stylized atom" />                             // here we have used the above image import.
     <h1>React Essentials</h1>
     <p>
-      Fundamental React concepts you will need for almost any app you are
+      {description} React concepts you will need for almost any app you are
       going to build!
     </p>
   </header>);
@@ -14,8 +23,8 @@ function Header() {
 function App() {
   return (
     <div>
-      <Header />             <------- // here we are using the upper Header component, we can also use the syntax <Header></Header> and <Header /> this is called self closing tag.
-      <main>
+      <Header />                          // here we are using the upper Header component, we can also use the syntax <Header></Header> and <Header /> this is called self closing tag.
+      <main>                                    
         <h2>Time to get started!</h2>
       </main>
     </div>
@@ -23,3 +32,12 @@ function App() {
 }
 
 export default App;
+
+
+
+
+
+
+
+                                                                    
+
