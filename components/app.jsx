2b6f62807f4a210e77269fac180c05355728218c
ipawanverma <148688699+ipawanverma@users.noreply.github.com>
2026-09-21 12:11:51 +0530

    Refactor comments for consistency in app.jsx
    Updated comments in app.jsx to use consistent formatting.

--- components/app.jsx
+++ components/app.jsx
@@ -1,5 +1,5 @@
-import reactImg from './assets/react-core-concepts.png';                   // reactImg is used for fetching the image from this location and then later on used below,
-                                                                           // also we can give any name of our choice like reactImg etc.
+import reactImg from './assets/react-core-concepts.png';                   //// reactImg is used for fetching the image from this location and then later on used below,
+                                                                           //// also we can give any name of our choice like reactImg etc.
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
 function genRandomInt(max) {
@@ -11,7 +11,7 @@ function Header() {
   const description = reactDescriptions[genRandomInt(2)];
 
   return (<header>
-    <img src={reactImg} alt="Stylized atom" />                             // here we have used the above image import.
+    <img src={reactImg} alt="Stylized atom" />                             //// here we have used the above image import.
     <h1>React Essentials</h1>
     <p>
       {description} React concepts you will need for almost any app you are
@@ -23,7 +23,7 @@ function Header() {
 function App() {
   return (
     <div>
-      <Header />                          // here we are using the upper Header component, we can also use the syntax <Header></Header> and <Header /> this is called self closing tag.
+      <Header />                          //// here we are using the upper Header component, we can also use the syntax <Header></Header> and <Header /> this is called self closing tag.
       <main>                                    
         <h2>Time to get started!</h2>
       </main>
