df88cc65f7c336e6fef424b2ea11904369a723aa
ipawanverma <148688699+ipawanverma@users.noreply.github.com>
2026-09-23 14:42:41 +0530

    Create initial App component with core concepts

---
+++ state/state.jsx
@@ -0,0 +1,49 @@
+// react hooks must only be called inside the component function or inside other react hooks
+
+
+import { useState } from 'react';                                // all these func which starts from use are react hooks
+import { CORE_CONCEPTS } from './data.js';
+import Header from './components/Header/Header.jsx';
+import CoreConcepts from './components/CoreConcepts.jsx';
+import TabButton from './components/TabButton.jsx';
+
+function App() {
+  let tabContent = 'Please click a button';
+  function handleSelect(selectedButton) {
+    // selectedButton => 'components', 'jsx', 'props, 'state'
+    tabContent = selectedButton;
+    console.log(selectedButton);
+  }
+
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcepts
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image} />
+            <CoreConcepts {...CORE_CONCEPTS[1]} />
+            <CoreConcepts {...CORE_CONCEPTS[2]} />
+            <CoreConcepts {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+          </menu>
+          {tabContent}
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
